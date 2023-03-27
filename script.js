8d6e171d57ec3fe7a4ec3be9081ac6249a9fa11e
Gabriel Oliveira <81538193+gabrielmatos51@users.noreply.github.com>
2023-03-27 20:23:11 -0300

    inserção das funções

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-var primeiroNum
+function insert(num)
+{
+    var numero = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = numero + num;
+}
+function clean()
+{
+    document.getElementById('screen').innerHTML = "";
+}
+function back()
+{
+    var resultado = document.getElementById('screen').innerHTML;
+    document.getElementById('screen').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular()
+{
+    var resultado = document.getElementById('screen').innerHTML;
+    if(resultado)
+    {
+        document.getElementById('screen').innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('screen').innerHTML = "Nada..."
+    }
+}
