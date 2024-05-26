7552d7660a90a986eec37427c4022b31d17c2712
Gabriel Oliveira <81538193+gabrielmatos51@users.noreply.github.com>
2024-05-26 01:28:43 -0300

    melhorias na exibição  dos numeros.

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ function calcular()
         document.getElementById('screen').innerHTML = "Nada..."
     }
 }
+
+function alterSize() {
+    var screenElement = document.getElementById('screen');
+    var contentLength = screenElement.innerHTML.length;
+
+    if (contentLength >= 7 && contentLength < 22) {
+        let fontSize = 85 - (contentLength * 3.2);
+        screenElement.style.fontSize = fontSize + 'px';
+        if(fontSize > 15){
+            screenElement.style.fontSize = fontSize + 'px';
+        }
+    } else if (contentLength >= 22 && contentLength < 50 ) {
+        let fontSize = 85 - (22 * 2.5) - ((contentLength - 22) * 2.8);
+
+        if(fontSize > 15){
+            screenElement.style.fontSize = fontSize + 'px';
+        }
+        
+    }
+    else {
+        screenElement.style.fontSize = '90px';
+    }
+}
+
+// Initial call to set the font size when the page loads
+alterSize();
+
+// Adjust font size on window resize
+window.addEventListener('resize', alterSize);
+
+// Adjust font size on content change (if applicable)
+new MutationObserver(alterSize).observe(document.getElementById('screen'), { characterData: true, childList: true, subtree: true });
