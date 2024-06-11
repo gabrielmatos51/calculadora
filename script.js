61ee35269fceb98ed1b55fedfbf471cfdadbe5a5
Gabriel Oliveira <81538193+gabrielmatos51@users.noreply.github.com>
2024-06-11 19:41:52 -0300

     Mwlhoria na exibição dos numeros 2.0

--- script.js
+++ script.js
@@ -31,18 +31,19 @@ function alterSize() {
 
     if (contentLength >= 7 && contentLength < 22) {
         let fontSize = 85 - (contentLength * 3.2);
-        screenElement.style.fontSize = fontSize + 'px';
-        if(fontSize > 15){
+        if (fontSize > 15) {
             screenElement.style.fontSize = fontSize + 'px';
         }
-    } else if (contentLength >= 22 && contentLength < 50 ) {
+    } else if (contentLength >= 22 && contentLength <= 36) {
         let fontSize = 85 - (22 * 2.5) - ((contentLength - 22) * 2.8);
-
-        if(fontSize > 15){
+        if (fontSize > 15) {
             screenElement.style.fontSize = fontSize + 'px';
         }
-        
+    } else if (contentLength >= 35  ) {
+        screenElement.innerHTML = "ERRO: QUANTITY NOT SUPPORTED";
+        screenElement.style.fontSize = '30PX';
     }
+    
     else {
         screenElement.style.fontSize = '90px';
     }
@@ -54,5 +55,9 @@ alterSize();
 // Adjust font size on window resize
 window.addEventListener('resize', alterSize);
 
-// Adjust font size on content change (if applicable)
+// Adjust font size on content change
 new MutationObserver(alterSize).observe(document.getElementById('screen'), { characterData: true, childList: true, subtree: true });
+// Initial call to set the font size when the page loads
+alterSize();
+
+ 
